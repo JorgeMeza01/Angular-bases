12cf5fa67a351de0a29a008a6ee8a31592d55b7e
Jorge <jorge_meza2018@outlook.es>
2023-12-28 19:18:23 -0600

    First commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public characterList:Character[] = [{
+    name: 'Trunks',
+    power:10
+  }]
+
+  // onDelete = Index value: number
+
+
+
+
+  onDeleteCharacter(id?:string):void{
+    // Emitir el ID del personaje
+    // console.log(character)
+    // console.log(index);
+
+    if(!id) return;
+
+    this.onDelete.emit(id);
+  }
+
+}
